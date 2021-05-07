c4a4bc606677a4c27c52642327ae87fa644078a9
Gabrielle Roth <gabrielle.roth@pgexperts.com>
2021-05-07 08:25:33 -0700

    initial stab at xid age check for replication slots
    not yet tested

---
+++ vacuum/replication_slot_xid_age.sql
@@ -0,0 +1,21 @@
+-- UNTESTED
+WITH slot_age AS (
+	SELECT slot_name, slot_type, active
+	, age(xmin) as xid_age
+	, age(catalog_xmin) AS catalog_xid_age
+	FROM pg_replication_slots
+),
+av_max_age AS (
+	SELECT setting::numeric AS max_age FROM pg_settings WHERE name = 'autovacuum_freeze_max_age'
+),
+wrap_pct AS (
+	SELECT slot_name, slot_type, active
+	, round(xid_age*100::numeric/max_age, 1) AS av_wrap_pct
+	, round(xid_age*100::numeric/2200000000, 1) as shutdown_pct
+	, round(catalog_xid_age*100::numeric/max_age, 1) AS av_wrap_pct_catalog
+	, round(catalog_xid_age*100::numeric/2200000000, 1) as shutdown_pct_catalog
+	FROM slot_age CROSS JOIN av_max_age
+)
+SELECT * FROM wrap_pct
+ORDER BY xid_age DESC
+;
