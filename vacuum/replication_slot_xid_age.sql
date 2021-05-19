a2ce68c74e0a321a60ca970ede6e16726052e2e2
Gabrielle Roth <gabrielle.roth@pgexperts.com>
2021-05-19 08:36:40 -0700

    add source data to help troubleshoot

--- vacuum/replication_slot_xid_age.sql
+++ vacuum/replication_slot_xid_age.sql
@@ -1,6 +1,8 @@
 -- UNTESTED
 WITH slot_age AS (
 	SELECT slot_name, slot_type, active
+	, xmin
+	, catalog_xmin
 	, age(xmin) as xid_age
 	, age(catalog_xmin) AS catalog_xid_age
 	FROM pg_replication_slots
@@ -10,6 +12,10 @@ av_max_age AS (
 ),
 wrap_pct AS (
 	SELECT slot_name, slot_type, active
+	, xmin
+	, catalog_xmin
+	, xid_age
+	, catalog_xid_age
 	, round(xid_age*100::numeric/max_age, 1) AS av_wrap_pct
 	, round(xid_age*100::numeric/2200000000, 1) as shutdown_pct
 	, round(catalog_xid_age*100::numeric/max_age, 1) AS av_wrap_pct_catalog
